7755096b4ceb529da283d1b413ec59907bcd9e9e
pottarino <pottarinii@gmail.com>
2026-04-03 00:32:40 +0200

    update

--- include/utility.h
+++ include/utility.h
@@ -5,5 +5,10 @@
 #ifndef SO2_UTILITY_H
 #define SO2_UTILITY_H
 void append_str(char** dest, const char* src);
-char* filereader(char* const filedest);
+char* filereader(char* filedest);
+const char** reserved_keywords;
+int is_keyword(char* name);
+int reserved_keywords_count;
+int custom_keywords_count;
+void add_reserved_keyword(char *new_keyword);
 #endif
